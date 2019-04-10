1e594109fa13ba52e6948305f165b45f48649b58
w262jxl <liu.111@wright.edu>
2019-04-10 18:57:40 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -24,30 +24,54 @@ shinyServer(
     
     #outputs
     observeEvent(input$searchBtn, {
-      output$searchOutput <- renderUI({
-        str1 <- "Company Searched:"
-        str2 <- isolate(toupper(input$searchInput))
-        HTML(paste(str1, str2, sep = ' '))
-      })
       
-      output$sheetTypeOutput <- renderUI({
-        str1 <- "Form(s) Selected:"
-        str2 <- isolate(input$checkboxGroup)
-        HTML(paste(str1, str2, sep=' '))
-      })
+      displayCompanySearched <- function(){
+        output$searchOutput <- renderUI({
+          str1 <- "Company Searched:"
+          str2 <- isolate(toupper(input$searchInput))
+          HTML(paste(str1, str2, sep = ' '))
+        })
+      }
       
-      output$datePickedOutput <- renderUI({
-        str1 <- "Years Selected:"
-        str2 <- isolate(input$datePicker)
-        str3 <- isolate(input$datePicker2)
-        HTML(paste(str1,str2,str3, sep='  '))
-      })
+      displaySheetTypeSelected <- function(sheet1, sheet2, sheet3){
+        if(is.na(sheet2) == TRUE){
+          str <- sheet1
+        }
+        else if(is.na(sheet3) == TRUE){
+          str <- paste(sheet1, "and", sheet2, sep = ' ')
+        }
+        else{
+          str <- paste(sheet1, ", ", sheet2, " and ", sheet3, sep = '')
+        }
+        output$sheetTypeOutput <- renderUI({
+          str2 <- "Sheet(s) Selected: "
+          HTML(paste(str2, str, sep = ''))
+        })
+      }
       
-      output$compInfo <- renderUI({
-        str <- isolate(CompanyInfo(input$searchInput))
-        HTML(paste(str, sep=''))
-      })
+      displayYearsSelected <- function(){
+        year1 <- input$datePicker
+        year2 <- input$datePicker2
+        
+        if(year1 == year2){
+          str <- year1
+        }
+        else{
+          str <- paste(year1, "-", year2, sep =' ')
+        }
+        
+        output$datePickedOutput <- renderUI({
+          str2 <- "Year(s) Selected: "
+          HTML(paste(str2, str, sep = ''))
+        })
+      }
       
+      displayCompanyInfo <- function(){
+        output$compInfo <- renderUI({
+          str <- isolate(CompanyInfo(input$searchInput))
+          HTML(paste(str, sep = ''))
+        })
+      }
       
       outputFileName <- reactive({
         isolatedSearchInput <- isolate(input$searchInput)
@@ -70,6 +94,36 @@ shinyServer(
           HTML(paste("The files are saved in the ", userInput, " folder.", sep = ''))
         })
       })
+      
+      noSheetSelectedMessage <- function(){
+        output$searchOutput <- renderUI({
+          str <- "Please select a sheet type"
+          HTML(paste(str, sep = ''))
+        })
+      }
+      
+      noCompanyNameMessage <- function(){
+        output$searchOutput <- renderUI({
+          str <- "Please enter a company stock symbol"
+          HTML(paste(str, sep=''))
+        })
+      }
+      
+      yearNotRightMessage <- function(){
+        output$searchOutput <- renderUI({
+          str <- "The end year must be the same or after the start year"
+          HTML(paste(str, sep=''))
+        })
+      }
+      
+      displayDownloadProgress <- function(count, fileCount){
+        output$downloadProgress <- renderUI({
+          count2 <- as.character(count)
+          str <- "out of"
+          str2 <- "files downloaded."
+          HTML(paste(count2, str, fileCount, str2, sep = ' '))
+        })
+      }
       
       getFileInfo <- function(form, year, company) {
         fileInfo <- c(form, year, company)
@@ -124,91 +178,122 @@ shinyServer(
         isolatedDate <- isolate(input$datePicker)
         isolatedDate2 <- isolate(input$datePicker2)
         year <- as.numeric(isolatedDate)
+        yearForIfCondition <- as.numeric(isolatedDate)
+        yearForIfCondition2 <- as.numeric(isolatedDate2)
         
         filesToDownload <- c()
         fileCount <- 0
         yearCount <- 1
+        status <- 1
         
-        while(year != isolatedDate2){
-          yearCount <- yearCount + 1
-          year <- year + 1
-        }
-        
-        if(is.null(isolatedSheetType[1]) == TRUE){
-          print("No sheet type selected")
-        }
-        else if(is.na(isolatedSheetType[2]) == TRUE){
-          selectedSheet1 <- isolatedSheetType[1]
-          yearPlaceholder <- isolatedDate
-          
-          for(i in 1: yearCount) {
-            newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            yearPlaceholder <- as.numeric(yearPlaceholder)
-            yearPlaceholder <- yearPlaceholder + 1
-            yearPlaceholder <- as.character(yearPlaceholder)
-            next
-          }
-        }
-        else if(is.na(isolatedSheetType[3]) == TRUE){
-          selectedSheet1 <- isolatedSheetType[1]
-          selectedSheet2 <- isolatedSheetType[2]
-          yearPlaceholder <- isolatedDate
-          
-          for(i in 1:yearCount) {
-            newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            newFile <- getFileInfo(selectedSheet2, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            yearPlaceholder <- as.numeric(yearPlaceholder)
-            yearPlaceholder <- yearPlaceholder + 1
-            yearPlaceholder <- as.character(yearPlaceholder)
-            next
-          }
+        if(yearForIfCondition > yearForIfCondition2){
+          status <- 0
+          print("The end year must be the same or after the start year")
+          yearNotRightMessage()
         }
         else{
-          selectedSheet1 <- isolatedSheetType[1]
-          selectedSheet2 <- isolatedSheetType[2]
-          selectedSheet3 <- isolatedSheetType[3]
-          yearPlaceholder <- isolatedDate
+          while(year != isolatedDate2){
+            yearCount <- yearCount + 1
+            year <- year + 1
+          }
           
-          for(i in 1:yearCount) {
-            newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            newFile <- getFileInfo(selectedSheet2, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            newFile <- getFileInfo(selectedSheet3, yearPlaceholder, isolatedSearchInput)
-            filesToDownload <- c(filesToDownload, newFile)
-            fileCount <- fileCount + 1
-            yearPlaceholder <- as.numeric(yearPlaceholder)
-            yearPlaceholder <- yearPlaceholder + 1
-            yearPlaceholder <- as.character(yearPlaceholder)
-            next
+          if(isolatedSearchInput == ""){
+            status <- 0
+            print("No company name entered")
+            noCompanyNameMessage()
+          }
+          else if(is.null(isolatedSheetType[1]) == TRUE){
+            status <- 0
+            print("No sheet type selected")
+            noSheetSelectedMessage()
+          }
+          else if(is.na(isolatedSheetType[2]) == TRUE){
+            selectedSheet1 <- isolatedSheetType[1]
+            yearPlaceholder <- isolatedDate
+            
+            for(i in 1: yearCount) {
+              newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              yearPlaceholder <- as.numeric(yearPlaceholder)
+              yearPlaceholder <- yearPlaceholder + 1
+              yearPlaceholder <- as.character(yearPlaceholder)
+              next
+            }
+          }
+          else if(is.na(isolatedSheetType[3]) == TRUE){
+            selectedSheet1 <- isolatedSheetType[1]
+            selectedSheet2 <- isolatedSheetType[2]
+            yearPlaceholder <- isolatedDate
+            
+            for(i in 1:yearCount) {
+              newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              newFile <- getFileInfo(selectedSheet2, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              yearPlaceholder <- as.numeric(yearPlaceholder)
+              yearPlaceholder <- yearPlaceholder + 1
+              yearPlaceholder <- as.character(yearPlaceholder)
+              next
+            }
+          }
+          else{
+            selectedSheet1 <- isolatedSheetType[1]
+            selectedSheet2 <- isolatedSheetType[2]
+            selectedSheet3 <- isolatedSheetType[3]
+            yearPlaceholder <- isolatedDate
+          
+            for(i in 1:yearCount) {
+              newFile <- getFileInfo(selectedSheet1, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              newFile <- getFileInfo(selectedSheet2, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              newFile <- getFileInfo(selectedSheet3, yearPlaceholder, isolatedSearchInput)
+              filesToDownload <- c(filesToDownload, newFile)
+              fileCount <- fileCount + 1
+              yearPlaceholder <- as.numeric(yearPlaceholder)
+              yearPlaceholder <- yearPlaceholder + 1
+              yearPlaceholder <- as.character(yearPlaceholder)
+              next
+            }
           }
         }
         
-        for (i in 1:fileCount) {
-          x <- (i - 1) * 3 + 1
-          print(x)
-          isolatedSheetType <- filesToDownload[c(x)]
-          x <- x + 1
-          isolatedDate <- filesToDownload[c(x)]
-          x <- x + 1
-          isolatedSearchInput <- filesToDownload[c(x)]
-          print(isolatedSheetType)
-          print(isolatedDate)
-          print(isolatedSearchInput)
-          csvFile <- downloadFile(isolatedSheetType, isolatedDate, isolatedSearchInput)
-          next
+        if(status == 0){
+          
         }
-        
-        read.csv(csvFile)
-        #downloadFile(isolatedSheetType, isolatedDate, isolatedSearchInput)
+        else{
+          count <- 0
+          displayDownloadProgress(count, fileCount)
+          displayCompanySearched()
+          displaySheetTypeSelected(isolatedSheetType[1], isolatedSheetType[2], isolatedSheetType[3])
+          displayYearsSelected()
+          displayCompanyInfo()
+          
+          for (i in 1:fileCount) {
+            x <- (i - 1) * 3 + 1
+            print(x)
+            isolatedSheetType <- filesToDownload[c(x)]
+            x <- x + 1
+            isolatedDate <- filesToDownload[c(x)]
+            x <- x + 1
+            isolatedSearchInput <- filesToDownload[c(x)]
+            print(isolatedSheetType)
+            print(isolatedDate)
+            print(isolatedSearchInput)
+            csvFile <- downloadFile(isolatedSheetType, isolatedDate, isolatedSearchInput)
+            count <- count + 1
+            displayDownloadProgress(count, fileCount)
+            next
+          }
+          
+          read.csv(csvFile)
+          #downloadFile(isolatedSheetType, isolatedDate, isolatedSearchInput)
+        }
       })
     })
     
@@ -258,9 +343,7 @@ shinyServer(
     output$filetable <- renderTable({
       filedata()
     })
-    
   }
-  
 )
   
 
